144eeece550e7753e9b2cf58e371b636a0a42526
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-26 22:16:33 +0100

    first commit

---
+++ Scholastify360/src/pages/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './About.css'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
+const About = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="about"></div>
+            <Footer />
+        </div>
+    )
+}
+
+export default About
---
+++ Scholastify360/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="main">
+
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
