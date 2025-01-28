cb16a23e306c2eafa85c50a47c4ba583f64430b7
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-28 20:49:43 +0100

    add all pages

--- Scholastify360/src/pages/About/About.jsx
+++ Scholastify360/src/pages/About/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './About.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 
---
+++ Scholastify360/src/pages/News/Event/News-event.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewsOrEvent = () => {
+    return (
+        <div>NewsOrEvent</div>
+    )
+}
+
+export default NewsOrEvent
