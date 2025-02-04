db1290509b5d651d409733fdd7eaa1e7a2870684
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-02-04 22:45:17 +0100

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Admission from './pages/Admission/Admission';
+import StudentAdmission from './pages/Admission/studentAdmission';
 import OnlinePayment from './pages/Payment/OnlinePayment';
 import Gallery from './pages/Gallery/Gallery';
 import NewsOrEvent from './pages/News/Event/News-event';
@@ -13,6 +14,7 @@ import Academics from './pages/Academics/Academics';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import BlogDetail from './pages/Blog/BlogDetail';
+import StudentRegistration from './pages/Admission/studentRegistration';
 
 const App = () => {
   const location = useLocation();
@@ -27,6 +29,8 @@ const App = () => {
         <Route path="/academics" element={<Academics />} />
         <Route path="/about" element={<About />} />
         <Route path="/admission" element={<Admission />} />
+        <Route path="/student-admission" element={<StudentAdmission />} />
+        <Route path="/student-registration" element={<StudentRegistration />} />
         <Route path="/payment" element={<OnlinePayment />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/news-event" element={<NewsOrEvent />} />
