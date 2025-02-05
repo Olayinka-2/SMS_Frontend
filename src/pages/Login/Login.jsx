1997cb5f8dff8cf9a09aebaf754663e811fefec1
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-02-05 17:47:00 +0100

    update login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,19 @@
-import './login.css'
+import { useState } from 'react';
+import './login.css';
+import { BsEye } from 'react-icons/bs';
+import { FaEye, FaEyeSlash } from 'react-icons/fa6';
 
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
         <div className="login-container" style={{ backgroundImage: `url('/login.png')` }}>
             <div className="login-container-overlay"></div>
-
             <div className="relative sm:items-center gap-10 z-10 flex md:flex-row flex-col md:items-start justify-center w-[100%] p-4">
-                {/* Logo and Welcome Message */}
-                <div className="text-white  flex flex-col justify-center items-center text-center">
+                <div className="text-white flex flex-col justify-center items-center text-center">
                     <img src="/logo.png" alt="Logo" className="w-34 h-34 md:mb-13" />
                     <h1 className="text-[65px] text-[#A5FFB9] font-bold">WELCOME</h1>
                 </div>
-
                 <div>
                     <div className="bg-white p-8 rounded-lg shadow-lg lg:w-[456px]">
                         <form>
@@ -20,22 +22,29 @@ const Login = () => {
                                     Email
                                 </label>
                                 <input
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    className="shadow appearance-none border border-[#989E99] rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="email"
                                     type="email"
                                     placeholder="Email here"
                                 />
                             </div>
-                            <div className="mb-6">
+                            <div className="mb-2 relative">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                                     Password
                                 </label>
                                 <input
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    className="shadow appearance-none border border-[#989E99] rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="password"
-                                    type="password"
+                                    type={showPassword ? "text" : "password"}
                                     placeholder="Password"
                                 />
+                                <button
+                                    type="button"
+                                    className="absolute right-3 top-10 text-gray-600"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                >
+                                    {showPassword ? <FaEye color='black' /> : <FaEyeSlash color='black' />}
+                                </button>
                             </div>
                             <div className="flex items-center justify-between mb-6">
                                 <a className="inline-block align-baseline font-bold text-sm hover:opacity-70 text-[#09B451]" href="#">
@@ -52,9 +61,7 @@ const Login = () => {
                             </div>
                         </form>
                     </div>
-
-                    {/* Copyright Message */}
-                    <div className=" text-white text-left p-2 text-[16px]">
+                    <div className="text-white text-left p-2 text-[16px]">
                         <p><span className='font-bold text-[20px] mr-2'>&copy; </span> {new Date().getFullYear()} Scholastify360 School Management System</p>
                     </div>
                 </div>
