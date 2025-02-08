66096634e87ab8dfbfcbaf7bb3180223203434d6
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-02-08 22:02:27 +0100

    hero

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import './Navbar.css'
-import { Link } from 'react-router-dom'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaReact, FaSearch } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
-        <div className='text-center'>
-            <div>logo</div>
-            <div className='flex gap-10'>
-                <Link to='/'>Home</Link>
-                <Link to='/academics'>Academics</Link>
-                <Link to='/about'>About us</Link>
-                <Link to='/admission'>Admission</Link>
-                <Link to='/payment'>Online Payment</Link>
-                <Link to='/gallery'>Gallery</Link>
-                <Link to='/news-event'>News/Event</Link>
-                <Link to='/blog'>Blog</Link>
-                <Link to='/contact'>Contact us</Link>
-                <div>
+        <div className='bg-white shadow-md py-4'>
+            <div className='flex justify-center items-center px-6'>
+                <div className='flex items-center text-xl font-bold gap-2'>
+                    <FaReact className='text-blue-500' />
+                    <span className='text-purple-600'>WiSchool</span>
+                </div>
+                <div className='flex items-center gap-6 mx-auto'>
+                    <Link to='/' className='hover:text-green-600'>Home</Link>
+                    <Link to='/academics' className='hover:text-green-600'>Academics</Link>
+                    <Link to='/about' className='hover:text-green-600'>About us</Link>
+                    <Link to='/admission' className='hover:text-green-600'>Admission</Link>
+                    <Link to='/payment' className='hover:text-green-600'>Online Payment</Link>
+                    <Link to='/gallery' className='hover:text-green-600'>Gallery</Link>
+                    <Link to='/news-event' className='hover:text-green-600'>News/Event</Link>
+                    <Link to='/blog' className='hover:text-green-600'>Blog</Link>
+                    <Link to='/contact' className='hover:text-green-600'>Contact us</Link>
+                </div>
+                <div className='flex items-center gap-3'>
+                    <FaSearch className='w-5 h-5 gap-3 text-purple-500 cursor-pointer' />
                     <Link to='/login'>
-                        <button>Login</button>
+                        <button className='bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition duration-300'>
+                            Login
+                        </button>
                     </Link>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Footer/Footer'
+import Hero from '../../components/Hero/Hero'
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcomr to <span>WiSchool</span>Where Learning Comes Alive!</h1>
-            <button>Apply here</button>
-
-            <div className="hero">
-                <h3>Expert instructors</h3>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+        <Hero />
+    </div>
+  )
 }
 
 export default Home
