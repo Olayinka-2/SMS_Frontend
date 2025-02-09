0be5a77caa1f30e476b22b243ee8072d7159dfd9
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-02-09 10:56:16 +0100

    update adm page

--- src/pages/Admission/Admission.jsx
+++ src/pages/Admission/Admission.jsx
@@ -15,18 +15,18 @@ const Admission = () => {
                     <h1 className="text-[45px] font-bold">Contact Us</h1>
                     <p className="mt-2 text-[24px]">Welcome to the WiSchool Admission page! We are excited that you are considering joining our vibrant learning community. Below, you will find information about our admission process, requirements, and how to apply. If you have any questions or need assistance, please don't hesitate to contact us.</p>
                     <div className="flex gap-5 mt-10">
-                        <Link to='/student-admission'><button className="bg-[#09B451] text-[#001B07] py-2 px-5">Student Admission</button></Link>
-                        <Link to='/student-registration'><button className="bg-[#FFFFFF] text-[#09B451] py-2 px-5">Student Registration</button></Link>
+                        <Link to='/student-admission'><button className="bg-[#09B451] rounded text-[#001B07] py-2 px-5">Student Admission</button></Link>
+                        <Link to='/student-registration'><button className="bg-[#FFFFFF] rounded text-[#09B451] py-2 px-5">Student Registration</button></Link>
                     </div>
                 </div>
             </div>
 
             <div className="mt-7 p-6 flex flex-col justify-center items-center">
-                <h3>Check Admission Status</h3>
+                <h3 className="text-[#001B07] text-[22px]">Check Admission Status</h3>
                 <div className="w-[50%]">
-                    <h4>Admission Number</h4>
-                    <div className="border flex border-[#E6E6E6] bg-[#F9F9F9] h-[40px] justify-between w-full">
-                        <input type="text" className="h-full w-full p-2" placeholder="Admission Number" />
+                    <h4 className="text-[#001B07] text-[16px] py-3">Admission Number</h4>
+                    <div className="border flex border-[#E6E6E6] rounded bg-[#F9F9F9] h-[40px] justify-between w-full">
+                        <input type="text" className="h-full p-2" placeholder="Admission Number" />
                         <button className="bg-[#09B451] p-2 h-full text-[#FFFFFF]">Check status</button>
                     </div>
                 </div>
