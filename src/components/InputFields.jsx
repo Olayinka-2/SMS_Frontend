34fd1a384b3485c96ed904b9af4f1700fa2f0e4f
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-02-09 12:31:56 +0100

    refactor input fields

---
+++ src/components/InputFields.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const InputFields = ({ title, placeholder }) => {
+    const [formData, setFormData] = useState({});
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+    return (
+        <label className="flex text-[#151716] text-[14px] font-semibold flex-col">
+            {title}
+            <input
+                type="text"
+                name="ad_number"
+                placeholder={placeholder}
+                onChange={handleChange}
+                className="p-2 border border-[#989E99] mt-2 rounded-md"
+            />
+        </label>
+    );
+};
+
+export default InputFields;
--- src/pages/Admission/studentAdmission.jsx
+++ src/pages/Admission/studentAdmission.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AdmissionHeader from "../../components/AdmissionHeader/header";
+import InputFields from "../../components/InputFields";
 
 const StudentAdmission = () => {
     const [formData, setFormData] = useState({});
@@ -19,11 +20,11 @@ const StudentAdmission = () => {
             <div className="w-[90%] px-14 py-2 bg-white">
                 <h2 className="text-[24.5px] font-bold mb-4 text-[#252B42]">Student Info</h2>
                 <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-y-4 gap-4">
-                    <label className="flex flex-col">Admission Number*
+                    {/* <label className="flex flex-col">Admission Number*
                         <input type="text" name="ad_number" placeholder="471" onChange={handleChange} className="p-2 border border-[#989E99] rounded-md" />
                     </label>
                     <label className="flex flex-col">Admission Date*
-                        <input type="date" name="date" placeholder="DATE" onChange={handleChange} className="p-2 border border-[#989E99] rounded-md" />
+                        <input type="date" name="date" onChange={handleChange} className="p-2 border border-[#989E99] rounded-md" />
                     </label>
                     <label className="flex flex-col">Class*
                         <select name="class" onChange={handleChange} className="p-2 border border-[#989E99] rounded-md">
@@ -142,8 +143,38 @@ const StudentAdmission = () => {
                     </label>
                     <label className="flex flex-col">Annual Income*
                         <input type="text" name="number" placeholder="income" onChange={handleChange} className="p-2 border border-[#989E99] rounded-md" />
-                    </label>
-                    <button type="submit" className=" bg-[#09B451] text-[#001B07] p-1 rounded mt-4">Submit</button>
+                    </label> */}
+                    <InputFields title="Admission Number*" placeholder="471" />
+                    <InputFields title="Admission Date*" placeholder="DD/MM/YY" />
+                    <InputFields title="Class*" placeholder="Select" />
+                    <InputFields title="First Name*" placeholder="First Name" />
+                    <InputFields title="Middle Name" placeholder="Middle Name" />
+                    <InputFields title="Last Name*" placeholder="Last Name" />
+                    <InputFields title="Date of Birth" placeholder="DD/MM/YY" />
+                    <InputFields title="Gender*" placeholder="Select" />
+                    <InputFields title="Address*" placeholder="Address" />
+                    <InputFields title="State*" placeholder="Select" />
+                    <InputFields title="Email*" placeholder="Email here" />
+                    <InputFields title="LGA*" placeholder="Select" />
+                    <InputFields title="Mobile number*" placeholder="Mobile number" />
+                    <InputFields title="Previous School*" placeholder="Previous School" />
+                    <h3 className="text-[25px] text-[#252B42] font-bold col-span-2 mt-4">Parent/Guardian Info</h3>
+                    <InputFields title="Title*" placeholder="Mr" />
+                    <InputFields title="First Name*" placeholder="First Name" />
+                    <InputFields title="Middle Name" placeholder="Middle Name" />
+                    <InputFields title="Last Name*" placeholder="Last Name" />
+                    <InputFields title="Date of Birth" placeholder="DD/MM/YY" />
+                    <InputFields title="Gender*" placeholder="Select" />
+                    <InputFields title="Address*" placeholder="Address" />
+                    <InputFields title="State*" placeholder="Select" />
+                    <InputFields title="LGA*" placeholder="Select" />
+                    <InputFields title="Email*" placeholder="Email here" />
+                    <InputFields title="Mobile number*" placeholder="Mobile number" />
+                    <InputFields title="Mobile number*" placeholder="Mobile number" />
+                    <InputFields title="Educational Qualification*" placeholder="Educational Qualification" />
+                    <InputFields title="Occupation" placeholder="Occupation" />
+                    <InputFields title="Annual Income" placeholder="Educational Qualification" />
+                    <button type="submit" className=" bg-[#09B451] text-[#001B07] p-1 rounded mt-7">Submit</button>
                 </form>
             </div>
         </>
