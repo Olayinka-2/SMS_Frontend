4b792043df933f09df7c48e652c5506ae5ea8ef8
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-02-12 21:14:55 +0100

    update btn

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,7 +13,7 @@ const HeroSection = ({ title, text, image, btn }) => {
                 <h1 className="text-[50px] font-bold">{title}</h1>
                 <p className="mt-2 text-[24px]">{text}</p>
                 {btn && (
-                    <div className="flex gap-5 mt-6">
+                    <div className="flex lg:flex-row flex-col md:flex-row gap-5 mt-6">
                         <Link to='/student-admission'><button className="bg-[#09B451] rounded text-[#001B07] py-2 px-5">Student Admission</button></Link>
                         <Link to='/student-registration'><button className="bg-[#FFFFFF] rounded text-[#09B451] py-2 px-5">Student Registration</button></Link>
                     </div>
