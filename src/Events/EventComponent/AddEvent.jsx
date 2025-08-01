1c1fe414fcec39ac2af8b58899423b01e336590c
olayinka-2 <olayinkaisrael802@gmail.com>
2025-08-01 13:59:00 +0100

    Adjust colors

--- src/Events/EventComponent/AddEvent.jsx
+++ src/Events/EventComponent/AddEvent.jsx
@@ -21,47 +21,47 @@ export default function AddEventPage() {
               <div className="font-bold text-lg mb-2">Basic Details</div>
               <form action="#">
                 <div className="flex flex-col gap-5">
-                  <div className="flex gap-1">
+                  <div className="flex gap-2">
                     <div className="flex-1 flex flex-col gap-1">
                       <label htmlFor="EventTitle" className="text-sm font-medium">Event Title*</label>
                       <input type="text" name="Title" id="EventTitle" placeholder="Event Title"
-                        className="border border-gray-300 rounded py-1 px-3 text-sm focus:outline-none"
+                        className="border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none"
                       />
                     </div>
                     <div className="flex-1 flex flex-col gap-1">
                       <label htmlFor="DOB" className="text-sm font-medium">Date of Birth*</label>
                       <input type="text" name="Title" id="DOB" placeholder="Data of Birth"
-                        className="border border-gray-300 rounded py-1 px-3 text-sm focus:outline-none" />
+                        className="border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none" />
                     </div>
                   </div>
                   <div className="flex gap-1">
                     <div className="flex-1 flex flex-col gap-1">
                       <label htmlFor="Time" className="text-sm font-medium">Time*</label>
                       <input type="text" name="Time" id="Time" placeholder="Time"
-                        className="border border-gray-300 rounded py-1 px-3 text-sm focus:outline-none"
+                        className="border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none"
                       />
                     </div>
                     <div className="flex-1 flex flex-col gap-1">
                       <label htmlFor="Location" className="text-sm font-medium">Location</label>
                       <input type="text" name="Location" id="Location" placeholder="Location"
-                        className="border border-gray-300 rounded py-1 px-3 text-sm focus:outline-none" />
+                        className="border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none" />
                     </div>
                   </div>
                   <div className="flex gap-1">
                     <div className="flex-1 flex flex-col gap-1">
                       <label htmlFor="Description" className="text-sm font-medium">Description*</label>
                       <input type="text" name="Description" id="Description" placeholder="Description"
-                        className="border border-gray-300 rounded py-1 px-3 text-sm focus:outline-none"
+                        className="border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none"
                       />
                     </div>
                     <div className="flex-1 flex flex-col gap-1">
                       <label htmlFor="Organizer" className="text-sm font-medium">Organizer*</label>
                       <input type="text" name="Organizer" id="Organizer" placeholder="Organizer"
-                        className="border border-gray-300 rounded py-1 px-3 text-sm focus:outline-none" />
+                        className="border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none" />
                     </div>
                   </div>
                   <div>
-                    <button className="py-1 px-4 bg-green-500 border border-green-500 rounded-sm  cursor-pointer hover:bg-green-600"
+                    <button className="py-1 px-4 bg-green-600 border border-green-500 rounded-sm  cursor-pointer hover:bg-green-500"
                     onClick={() => navigate("/Events")}
                     >Add Event</button>
                   </div>
